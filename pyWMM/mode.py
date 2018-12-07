1c304b574a126d73c7a8f8bcfd9536278ab99908
ece student <alec.m.hammond@gmail.com>
2018-12-07 12:54:25 -0700

    progress

--- pyWMM/mode.py
+++ pyWMM/mode.py
@@ -12,7 +12,7 @@
 # ---------------------------------------------------------------------------- #
 #
 # ---------------------------------------------------------------------------- #
-import pyWMM as wmm
+from pyWMM import WMM as wmm
 import numpy as np
 from scipy import interpolate
 from scipy import integrate
@@ -26,43 +26,60 @@ from scipy import integrate
 '''
 class Mode:
 
-    def __init__(self, Eps, Eps_cladding,
-                 Ex = None, Ey, Ez = None, Er = None, Ephi = None,
-                 Hx = None, Hy, Hz = None, Hr = None, Hphi = None,
-                 x = None, y, r = None, radius = None, center,
-                 kVec, pol = None, wavelength = None, neff = None
+    def __init__(self, Eps, kVec, center,
+                 Ex = None, Ey = None, Ez = None, Er = None, Ephi = None,
+                 Hx = None, Hy = None, Hz = None, Hr = None, Hphi = None,
+                 x = None, y = None, r = None, radius = None,
+                 pol = None, wavelength = None, neff = None
                  ):
 
         # Ensure the correct coordinate system
-        if (Ex == None and Er == None and Ez == None and Ephi == none)
-        or (Hx == None and Hr == None and Hz == None and Hphi == none)
-        or (x == None and r == None):
+        if (Ex is None and Er is None and Ez is None and Ephi is None) or (Hx is None and Hr is None and Hz is None and Hphi is None) or (x is None and r is None):
             raise ValueError('Missing either an x component or r component in fields!')
-        elif Ex == None and Hx == None and x == None:
+        elif Ex is None and Hx is None and x is None:
             self.coordinates = wmm.CYLINDRICAL
-        elif Er == None and Hr == None and r == None:
+        elif Er is None and Hr is None and r is None:
             self.coordinates = wmm.CARTESIAN
         else:
             raise ValueError('Invalid coordinate system defined!')
 
-        if self.coordinates = wmm.CYLINDRICAL and radius == None:
+        if self.coordinates == wmm.CYLINDRICAL and radius == None:
             raise ValueError('Failed to specify the waveguide\'s center radius!')
 
         # Interpolate all of the fields on the given grid
         self.y = y
-        if self.coordinates = wmm.CARTESIAN:
-            self.Eps          = interpolate.RectBivariateSpline(x, y, Eps)
-            self.Eps_cladding = interpolate.RectBivariateSpline(x, y, Eps)
-            self.Ex           = interpolate.RectBivariateSpline(x, y, Ex)
-            self.Ey           = interpolate.RectBivariateSpline(x, y, Ey)
-            self.Ez           = interpolate.RectBivariateSpline(x, y, Ez)
-            self.Hx           = interpolate.RectBivariateSpline(x, y, Hx)
-            self.Hy           = interpolate.RectBivariateSpline(x, y, Hy)
-            self.Hz           = interpolate.RectBivariateSpline(x, y, Hz)
+        if self.coordinates == wmm.CARTESIAN:
+            self.Eps_r          = interpolate.RectBivariateSpline(x, y, np.real(Eps))
+            self.Eps_i          = interpolate.RectBivariateSpline(x, y, np.imag(Eps))
+            self.Eps = lambda x,y: self.Eps_r(x,y) + 1j*self.Eps_i(x,y)
+
+            self.Ex_r           = interpolate.RectBivariateSpline(x, y, np.real(Ex))
+            self.Ex_i           = interpolate.RectBivariateSpline(x, y, np.imag(Ex))
+            self.Ex = lambda x,y: self.Ex_r(x,y) + 1j*self.Ex_i(x,y)
+
+            self.Ey_r           = interpolate.RectBivariateSpline(x, y, np.real(Ey))
+            self.Ey_i           = interpolate.RectBivariateSpline(x, y, np.imag(Ey))
+            self.Ey = lambda x,y: self.Ey_r(x,y) + 1j*self.Ey_i(x,y)
+
+            self.Ez_r           = interpolate.RectBivariateSpline(x, y, np.real(Ez))
+            self.Ez_i           = interpolate.RectBivariateSpline(x, y, np.imag(Ez))
+            self.Ez = lambda x,y: self.Ez_r(x,y) + 1j*self.Ez_i(x,y)
+
+            self.Hx_r           = interpolate.RectBivariateSpline(x, y, np.real(Hx))
+            self.Hx_i           = interpolate.RectBivariateSpline(x, y, np.imag(Hx))
+            self.Hx = lambda x,y: self.Hx_r(x,y) + 1j*self.Hx_i(x,y)
+
+            self.Hy_r           = interpolate.RectBivariateSpline(x, y, np.real(Hy))
+            self.Hy_i           = interpolate.RectBivariateSpline(x, y, np.imag(Hy))
+            self.Hy = lambda x,y: self.Hy_r(x,y) + 1j*self.Hy_i(x,y)
+
+            self.Hz_r           = interpolate.RectBivariateSpline(x, y, np.real(Hz))
+            self.Hz_i           = interpolate.RectBivariateSpline(x, y, np.imag(Hz))
+            self.Hz = lambda x,y: self.Hz_r(x,y) + 1j*self.Hz_i(x,y)
+
             self.x            = x
-        elif self.coordinates = wmm.CYLINDRICAL:
+        elif self.coordinates == wmm.CYLINDRICAL:
             self.Eps          = interpolate.RectBivariateSpline(r, y, Eps)
-            self.Eps_cladding = interpolate.RectBivariateSpline(r, y, Eps_cladding)
             self.Er           = interpolate.RectBivariateSpline(r, y, Er)
             self.Ey           = interpolate.RectBivariateSpline(r, y, Ey)
             self.Ephi         = interpolate.RectBivariateSpline(r, y, Ephi)
@@ -83,17 +100,17 @@ class Mode:
         self.ampfac = np.sqrt(self.nrm)/np.sqrt(self.total_power)
 
         #Initialize all the other variables
-    	self.pol  = None
-    	self.sym  = None
-    	self.beta = None
-    	self.k0   = None
-    	self.neff = None
+        self.pol  = None
+        self.sym  = None
+        self.beta = None
+        self.k0   = None
+        self.neff = None
         self.center = center
 
 	# field values at point (x, y)
 	# 	Fcomp: E, H, Eps
     # returns a #D vector
-	def get_field(self, fComp, x, y, z):
+    def get_field(self, fComp, x, y, z):
 
         #Extract center
         centerX = self.center[0]
@@ -101,7 +118,7 @@ class Mode:
         centerZ = self.center[2]
 
         if fComp == wmm.E:
-            if self.coordinates = wmm.CYLINDRICAL:
+            if self.coordinates == wmm.CYLINDRICAL:
                 r = np.sqrt((x - centerX) ** 2 + (z - centerZ) ** 2)
                 Er   = self.Er(r,(y-centerY))
                 Ey   = self.Ey(r,(y-centerY))
@@ -112,10 +129,10 @@ class Mode:
                 Ex = self.Ex((x-centerX),(y-centerY))
                 Ey = self.Ey((x-centerX),(y-centerY))
                 Ez = self.Ez((x-centerX),(y-centerY))
-            E = np.array([Ex,Ey,Ez])
+            E = np.array([Ex,Ey,Ez]) * self.ampfac
             return E
         elif fComp == wmm.H:
-            if self.coordinates = wmm.CYLINDRICAL:
+            if self.coordinates == wmm.CYLINDRICAL:
                 r = np.sqrt((x - centerX) ** 2 + (z - centerZ) ** 2)
                 Hr   = self.Hr(r,(y-centerY))
                 Hy   = self.Hy(r,(y-centerY))
@@ -126,25 +143,25 @@ class Mode:
                 Hx = self.Hx((x-centerX),(y-centerY))
                 Hy = self.Hy((x-centerX),(y-centerY))
                 Hz = self.Hz((x-centerX),(y-centerY))
-            H = np.array([Hx,Hy,Hz])
+            H = np.array([Hx,Hy,Hz]) * self.ampfac
             return H
         elif fComp == wmm.Eps:
-            if self.coordinates = wmm.CYLINDRICAL:
+            if self.coordinates == wmm.CYLINDRICAL:
                 r = np.sqrt((x - centerX) ** 2 + (z - centerZ) ** 2)
                 Eps = self.Eps(r,(y-centerY))
             else:
-                Eps = self.((x-centerX),(y-centerY))
-        else
+                Eps = self.Eps((x-centerX),(y-centerY))
+        else:
             raise ValueError("Invalid component specified!")
 
 	# longitudinal component of the Poyntingvector,
 	# integrated over the entire x-y-domain
 	# TE part
-	def calc_TE_power(self):
-        if self.coordinates = wmm.CARTESIAN:
+    def calc_TE_power(self):
+        if self.coordinates == wmm.CARTESIAN:
             f = lambda y, x: self.Ey(x,y) * self.Hx(x,y)
             x0 = self.x[0]; x1 = self.x[-1];
-        elif self.coordinates = wmm.CYLINDRICAL:
+        elif self.coordinates == wmm.CYLINDRICAL:
             f = lambda y, r: self.Ey(x,y) * self.Hr(r,y)
             x0 = self.r[0]; x1 = self.r[-1];
         else:
@@ -155,11 +172,11 @@ class Mode:
 	# longitudinal component of the Poyntingvector,
 	# integrated over the entire x-y-domain
 	# TM part
-	def calc_TM_power():
-        if self.coordinates = wmm.CARTESIAN:
+    def calc_TM_power():
+        if self.coordinates == wmm.CARTESIAN:
             f = lambda y, x: self.Ex(x,y) * self.Hy(x,y)
             x0 = self.x[0]; x1 = self.x[-1];
-        elif self.coordinates = wmm.CYLINDRICAL:
+        elif self.coordinates == wmm.CYLINDRICAL:
             f = lambda y, r: self.Ex(x,y) * self.Hy(r,y)
             x0 = self.r[0]; x1 = self.r[-1];
         else:
